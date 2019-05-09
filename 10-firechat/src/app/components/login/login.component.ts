fc69d1593d4559354e1ef7183ab6fbac8cf9d78f
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-05-09 10:49:39 -0500

    10 FIRECHAT

---
+++ 10-firechat/src/app/components/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from "../../services/chat.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent implements OnInit {
+
+  constructor( private _cs:ChatService ) {
+
+  };
+
+  ngOnInit() {
+  }
+
+  login( proveedor:string ){
+    this._cs.login( proveedor );
+  };
+
+}
---
+++ 10-firechat/src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyAieGFiYhK0y3TYG9adUX1D9C6oaDW7PIk",
+  authDomain: "firechat-556e7.firebaseapp.com",
+  databaseURL: "https://firechat-556e7.firebaseio.com",
+  projectId: "firechat-556e7",
+  storageBucket: "firechat-556e7.appspot.com",
+  messagingSenderId: "791364284970"
+};
