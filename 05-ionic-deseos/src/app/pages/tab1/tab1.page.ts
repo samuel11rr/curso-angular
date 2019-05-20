2e5bcd9cd0bda60f533052e6f174b1a72fae89eb
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-05-20 09:43:53 -0500

    AVANCES A LA CREACION DE LISTAS EN IONIC

--- 05-ionic-deseos/src/app/pages/tab1/tab1.page.ts
+++ 05-ionic-deseos/src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -8,8 +9,15 @@ import { DeseosService } from '../../services/deseos.service';
 })
 export class Tab1Page {
 
-  constructor( public deseosService: DeseosService ) {
+  constructor( public deseosService: DeseosService,
+               private router: Router ) {
     
+  }
+
+  agregarLista(){
+
+    this.router.navigateByUrl('/tabs/tab1/agregar');
+
   }
 
 }
