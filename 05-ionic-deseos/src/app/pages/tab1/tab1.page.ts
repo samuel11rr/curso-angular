0f105f25f9eb6767bf09b0a30fab8b0393eeae5a
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-05-27 17:52:09 -0500

    AVANCE EN CREACION DE LISTAS

--- 05-ionic-deseos/src/app/pages/tab1/tab1.page.ts
+++ 05-ionic-deseos/src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -10,13 +11,44 @@ import { Router } from '@angular/router';
 export class Tab1Page {
 
   constructor( public deseosService: DeseosService,
-               private router: Router ) {
-    
+               private router: Router,
+               private alertCtrl: AlertController ) {
+
   }
 
-  agregarLista(){
+  async agregarLista(){
 
-    this.router.navigateByUrl('/tabs/tab1/agregar');
+    // this.router.navigateByUrl('/tabs/tab1/agregar');
+    const alert = await this.alertCtrl.create({
+      header: 'Nueva lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('cancelar');
+          }
+        },
+        {
+          text: 'Crear',
+          handler: ( data ) =>{
+            console.log(data);
+            if ( data.titulo.length === 0 ) return;
+
+            this.deseosService.crearLista(data.titulo);
+          }
+        }
+      ]
+    });
+
+    alert.present();
 
   }
 
--- 05-ionic-deseos/src/app/services/deseos.service.ts
+++ 05-ionic-deseos/src/app/services/deseos.service.ts
@@ -18,4 +18,12 @@ export class DeseosService {
 
     this.listas.push(lista1, lista2);
   }
+
+  crearLista( titulo:String ){
+    const nuevaLista = new Lista(titulo);
+
+    this.listas.push( nuevaLista );
+  }
+
+
 }
