36610d2122eed803cb7eca696a469fe8461ba7f3
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-07-08 08:49:49 -0500

    AGREGAMOS TAREAS POR LISTA

--- 05-ionic-deseos/src/app/pages/tab1/tab1.page.ts
+++ 05-ionic-deseos/src/app/pages/tab1/tab1.page.ts
@@ -42,7 +42,10 @@ export class Tab1Page {
             console.log(data);
             if ( data.titulo.length === 0 ) return;
 
-            this.deseosService.crearLista(data.titulo);
+            const listaId = this.deseosService.crearLista(data.titulo);
+
+            this.router.navigateByUrl(`/tabs/tab1/agregar/${ listaId }`)
+
           }
         }
       ]
