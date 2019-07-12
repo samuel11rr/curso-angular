195a43ce6f1406dfbbd8718c1d23ba17ee53329b
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-07-12 10:01:20 -0500

    IMPLEMENTACION DE COMPONENTE PARA LISTAS

--- 05-ionic-deseos/src/app/pages/tab1/tab1.page.ts
+++ 05-ionic-deseos/src/app/pages/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Lista } from 'src/app/models/lista.model';
 
 @Component({
   selector: 'app-tab1',
@@ -53,6 +54,11 @@ export class Tab1Page {
 
     alert.present();
 
+  }
+
+
+  listaSeleccionada( lista: Lista ) {
+    this.router.navigateByUrl(`/tabs/tab1/agregar/${ lista.id }`);
   }
 
 }
