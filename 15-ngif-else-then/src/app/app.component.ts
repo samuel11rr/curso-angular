f2432e09e5d9357f25222f37a918a55575dfe5aa
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-08-15 12:49:34 -0500

    15 NG IF THEN ELSE
    Ejercicio rapido sobre ngIf con then y else

---
+++ 15-ngif-else-then/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ngif-else-then';
+  usuario = null;
+}
