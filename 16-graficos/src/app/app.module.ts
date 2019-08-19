8c26485e5441f17badd45ddb1702a139c6690563
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-08-18 19:00:57 -0500

    16 GRAFICOS APP

---
+++ 16-graficos/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { ChartsModule } from 'ng2-charts';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarrasComponent } from './components/barras/barras.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LineaComponent,
+    BarrasComponent,
+    DonaComponent,
+    RadarComponent
+  ],
+  imports: [
+    BrowserModule,
+    ChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
