078b09f4e2f60ccbc67250250b0e97f354b267e8
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-10-26 21:35:11 -0500

    18 PORTAFOLIO TERMINADO
    Se hizo despliegue en firebase, la configuracion de firebase se agrego a .gitignore

--- 18-portafolio/src/app/services/productos.service.ts
+++ 18-portafolio/src/app/services/productos.service.ts
@@ -9,7 +9,8 @@ export class ProductosService {
 
   url = 'https://angular-portafolio-bc133.firebaseio.com';
   cargando = true;
-  productos: Producto[];
+  productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) {
     this.cargarProductos();
@@ -17,10 +18,49 @@ export class ProductosService {
 
 
   private cargarProductos() {
-    this.http.get( `${ this.url }/productos_idx.json` )
-    .subscribe( (resp: Producto[]) => {
-      this.productos = resp;
-      this.cargando = false;
+
+    return new Promise( ( resolve, reject ) => {
+      this.http.get( `${ this.url }/productos_idx.json` )
+      .subscribe( (resp: Producto[]) => {
+        this.productos = resp;
+        this.cargando = false;
+        resolve();
+      });
     });
+
+  }
+
+
+  getProducto( id: string ) {
+    const url = 'https://angular-portafolio-bc133.firebaseio.com/productos';
+    return this.http.get( `${ url }/${ id }.json` );
+  }
+
+
+  buscarProducto( termino: string ) {
+    if ( this.productosFiltrado.length === 0 ) {
+      this.cargarProductos().then( () => {
+        this.filtrarProductos( termino );
+      });
+    }
+    else{
+      this.filtrarProductos( termino );
+    }
+  }
+
+
+  private filtrarProductos( termino: string ) {
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLowerCase();
+
+      if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push( prod );
+      }
+
+    });
+
   }
 }
